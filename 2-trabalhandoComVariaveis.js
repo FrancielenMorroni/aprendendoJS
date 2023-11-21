92f8cdf76c4da9ce03ac8413cf6bdaba82add3fb
FrancielenMorroni <93045430+FrancielenMorroni@users.noreply.github.com>
2023-11-20 22:50:46 -0300

    Aprendendo sobre JavaScript

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com variáveis");
+
+const idade = 29;
+const nome = "Ricardo";
+
+//não fazer
+//mes = "Janeiro";
+
+console.log(idade);
+
---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,24 @@
+console.log("Atribuindo Variaveis");
+
+const nome = "Ricardo";
+const sobrenome = "Bugan";
+
+//console.log(nome + " " + sobrenome);
+console.log(nome, sobrenome);
+
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+
+let contador = 0;
+contador = contador + 1;
+
+console.log(contador)
+
+let idade;
+idade = 26;
+idade = idade + 1;
+
+console.log(idade);
